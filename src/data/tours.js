50fb1c486309bac2e94844247dd13133516f04b9
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-06-22 17:53:14 +0300

    Hard coding improvement.

---
+++ src/data/tours.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    title: 'Spain',
+    price: '800$',
+    description: '1 Week in Spain',
+    url: '/tours/spain',
+  },
+  {
+    title: 'France',
+    price: '800$',
+    description: '1 Week in France',
+    url: '/tours/france',
+  },
+  {
+    title: 'Germany',
+    price: '800$',
+    description: '1 Week in Germany',
+    url: '/tours/germany',
+  },
+  {
+    title: 'Greece',
+    price: '800$',
+    description: '1 Week in Greece',
+    url: '/tours/greece',
+  },
+  {
+    title: 'Japan',
+    price: '800$',
+    description: '1 Week in Japan',
+    url: '/tours/japan',
+  },
+  {
+    title: 'USA',
+    price: '800$',
+    description: '1 Week in USA',
+    url: '/tours/usa',
+  },
+];
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,6 +6,7 @@ import { Heading1 } from '../components/Variants';
 import Layout from '../layouts/Layout';
 import SEO from '../components/SEO';
 import TourCard from '../components/TourCard';
+import tourData from '../data/tours';
 
 // eslint-disable-next-line react/prop-types
 const IndexPage = ({ data }) => {
@@ -21,48 +22,20 @@ const IndexPage = ({ data }) => {
           Available Tours
         </Heading1>
         <Flex flexWrap="wrap" style={{ maxWidth: '1200px', margin: '0 auto' }}>
-          <TourCard
-            title="Spain"
-            fluidImage={tours.find(({ originalName }) => originalName.match(/spain/i)).image}
-            price="800$"
-            description="1 Week in Spain"
-            url="/tours/spain"
-          />
-          <TourCard
-            title="France"
-            fluidImage={tours.find(({ originalName }) => originalName.match(/France/i)).image}
-            price="800$"
-            description="1 Week in France"
-            url="/tours/france"
-          />
-          <TourCard
-            title="Germany"
-            fluidImage={tours.find(({ originalName }) => originalName.match(/Germany/i)).image}
-            price="800$"
-            description="1 Week in Germany"
-            url="/tours/germany"
-          />
-          <TourCard
-            title="Greece"
-            fluidImage={tours.find(({ originalName }) => originalName.match(/Greece/i)).image}
-            price="800$"
-            description="1 Week in Greece"
-            url="/tours/greece"
-          />
-          <TourCard
-            title="Japan"
-            fluidImage={tours.find(({ originalName }) => originalName.match(/Japan/i)).image}
-            price="800$"
-            description="1 Week in Japan"
-            url="/tours/japan"
-          />
-          <TourCard
-            title="USA"
-            fluidImage={tours.find(({ originalName }) => originalName.match(/USA/i)).image}
-            price="800$"
-            description="1 Week in USA"
-            url="/tours/usa"
-          />
+          {tourData.map(({
+            title, price, description, url,
+          }) => {
+            const regExp = new RegExp(title, 'i');
+            return (
+              <TourCard
+                title={title}
+                fluidImage={tours.find(({ originalName }) => originalName.match(regExp)).image}
+                price={price}
+                description={description}
+                url={url}
+              />
+            );
+          })}
         </Flex>
       </Box>
     </Layout>
